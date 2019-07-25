a6e294226a0ca806b08eb2e89ecd2369dd24fda6
OkuboHeavyIndustries <53265107+OkuboHeavyIndustries@users.noreply.github.com>
2019-07-25 22:03:50 +0900

    Update SSD1306_3D_ELITE.ino

--- SSD1306_3D_ELITE.ino
+++ SSD1306_3D_ELITE.ino
@@ -1,7 +1,7 @@
 /*
  * Based on an idea of https://www.reddit.com/r/EliteDangerous/comments/bjdg54/old_school_elite_ship_viewer/
  * 
- * Code base on http://colinord.blogspot.com/2015/01/arduino-oled-module-with-3d-demo.html
+ * Code based on http://colinord.blogspot.com/2015/01/arduino-oled-module-with-3d-demo.html
  */
 
 
